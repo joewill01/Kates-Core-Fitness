83f0e8d0dba767a8e1e92e6e0fbba96172258190
joewill01 <joew4119@gmail.com>
2019-09-16 17:28:52 +0100

    Update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 60) {
+        $(".nav-holder").addClass("nav-holder-onscroll");
+        $(".nav-logo").addClass("nav-logo-onscroll");
+    } else if($(window).scrollTop() == 0) {
+        $(".nav-holder").removeClass("nav-holder-onscroll");
+        $(".nav-logo").removeClass("nav-logo-onscroll");
+    }
+});
