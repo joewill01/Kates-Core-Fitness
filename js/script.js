469336b57941b50fc82b32305e3962b4c3182b5b
joewill01 <joew4119@gmail.com>
2019-09-16 21:09:14 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 60) {
         $(".nav-holder").addClass("nav-holder-onscroll");
+        $(".nav-holder-mobile").addClass("nav-holder-onscroll");
         $(".nav-logo").addClass("nav-logo-onscroll");
         $(".nav-link").addClass("nav-link-onscroll");
     } else if($(window).scrollTop() < 60) {
         $(".nav-holder").removeClass("nav-holder-onscroll");
+        $(".nav-holder-mobile").removeClass("nav-holder-onscroll");
         $(".nav-logo").removeClass("nav-logo-onscroll");
         $(".nav-link").removeClass("nav-link-onscroll");
     }
 });
+
+if ($(window).width() < 550) {
+     $(".nav-holder-mobile").css("display", "block");
+     $(".nav-holder").css("display", "none");
+  }
+ else {
+    $(".nav-holder-mobile").css("display", "none");
+     $(".nav-holder").css("display", "block");
+ }
+
+$(window).resize(function() {
+  if ($(window).width() < 550) {
+     $(".nav-holder-mobile").css("display", "block");
+     $(".nav-holder").css("display", "none");
+  }
+ else {
+    $(".nav-holder-mobile").css("display", "none");
+     $(".nav-holder").css("display", "block");
+ }
+});
+
+ function hideMenu() {
+     $("#hamburger-toggle").prop("checked", false);
+ }
