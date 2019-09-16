f30d2f57a7ffe088abb5d214144cc4dcd63c7402
joewill01 <joew4119@gmail.com>
2019-09-16 18:29:53 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(window).on("scroll", function() {
     if($(window).scrollTop() > 60) {
         $(".nav-holder").addClass("nav-holder-onscroll");
         $(".nav-logo").addClass("nav-logo-onscroll");
-    } else if($(window).scrollTop() == 0) {
+    } else if($(window).scrollTop() < 60) {
         $(".nav-holder").removeClass("nav-holder-onscroll");
         $(".nav-logo").removeClass("nav-logo-onscroll");
     }
