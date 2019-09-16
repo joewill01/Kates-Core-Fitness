2de93a55216d23d90acd402ced021419900f00a3
joewill01 <joew4119@gmail.com>
2019-09-16 20:03:19 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ $(window).on("scroll", function() {
     if($(window).scrollTop() > 60) {
         $(".nav-holder").addClass("nav-holder-onscroll");
         $(".nav-logo").addClass("nav-logo-onscroll");
+        $(".nav-link").addClass("nav-link-onscroll");
     } else if($(window).scrollTop() < 60) {
         $(".nav-holder").removeClass("nav-holder-onscroll");
         $(".nav-logo").removeClass("nav-logo-onscroll");
+        $(".nav-link").removeClass("nav-link-onscroll");
     }
 });
+
